0ec79d380a948c98a927f0fcfd8c3576dcca4b97
Claire Thompson <clairethompson@Claires-MacBook-Pro.local>
2022-03-08 09:50:17 -0500

    dispo routes and controllers

---
+++ controllers/dispositions.js
@@ -0,0 +1,1 @@
+import { Disposition } from "../models/disposition";
---
+++ routes/dispositions.js
@@ -0,0 +1,3 @@
+import { Router } from "express"
+import { isLoggedIn } from '../middleware/middleware.js'
+import * as disposCtrl from "../controllers/dispositions.js"
